8b174e275b2af3e41dd73464ab5509b9d1ea2b5a
Vis <vt201916384@gmail.com>
2023-04-11 23:58:54 -0400

    basic navbar setup

---
+++ store-assistant-website/src/Components/Nav/nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div className='w-full h-24 bg-slate-200 flex justify-center divide-x-4 divide-slate-400 items-center gap-10'>
+            <p className="text-3xl font-semibold">Store Assist</p>
+        </div>
+    )
+}
