ef32bd435ed593bc81a2d82e4c761ce6eefbcc79
Vis <vt201916384@gmail.com>
2023-04-15 19:03:42 -0400

    basic landing page setup

--- store-assistant-website/src/pages/index.tsx
+++ store-assistant-website/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import Nav from "~/Components/Nav/nav";
-import raleway from "~/utils/fonts";
+import { raleway } from "~/utils/fonts";
 
 import { api } from "~/utils/api";
 
@@ -19,6 +20,26 @@ const Home: NextPage = () => {
         </Head>
         <main className="flex min-h-screen flex-col bg-white">
             <Nav/>
+            <div className={`h-1/2 p-10 w-full bg-slate-200 flex flex-row font-sans ${raleway.variable} py-20`}>
+              <div className="w-1/2 flex justify-center items-center flex-col gap-5">
+                <p className="font-semibold text-5xl hover:-translate-y-1 duration-300">Create AI Store Assistants in Minutes</p>
+                <p className="text-lg font-light text-center w-[70%] hover:-translate-y-1 duration-300">Leveraging AI technology to create smart store assistants to make it easier for customers to find products, including people with a language barrier! Providing technology interfaces to real-world store locations</p>
+                <button className='w-[30%] h-16 text-white bg-violet-500 text-2xl font-normal disabled:cursor-not-allowed cursor-pointer disabled:hover:bg-violet-50 rounded-md disabled:bg-violet-100 enabled:hover:bg-violet-600 enabled:hover:text-gray-200 enabled:hover:-translate-y-1 enabled:hover:shadow-md duration-300'>Get Started</button>
+              </div>
+              <div className="w-1/2 flex justify-start">
+                <Image src="/undraw_shopping_app_flsj.svg" alt="bruh" width={650} height={100} className="ml-[10%] hover:-translate-y-1 duration-300"/>
+              </div>
+            </div>
+            <div className={`p-10 w-full flex flex-row font-sans ${raleway.variable} py-20`}>
+              <div className="w-1/2 flex justify-center">
+                <Image src="/undraw_shopping_bags_tdby.svg" alt="bruh" width={700} height={100} className="hover:-translate-y-1 duration-300"/>
+              </div>
+              <div className="w-1/2 flex flex-col justify-center items-center gap-10">
+                <p className="text-6xl">Very easy to use</p>
+                <p className="text-6xl">Multiple languages</p>
+                <p className="text-6xl">Easy to integrate</p>
+              </div>
+            </div>
         </main>
     </>
   );
--- store-assistant-website/src/pages/signup.tsx
+++ store-assistant-website/src/pages/signup.tsx
@@ -25,7 +25,7 @@ export default function Login() {
             <Nav/>
             <div className={`w-1/2 mx-[25%] h-1/2 my-[10%] rounded-md flex bg-white items-center flex-col ${raleway.variable} font-sans gap-10`}>
                 {/* <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa, className: { container: 'w-full h-full' } }} theme="light" redirectTo='http://localhost:3000/'/> */}
-                <h1 className="text-6xl text-slate-800">Sign in</h1>
+                <h1 className="text-6xl text-slate-800">Sign Up</h1>
                 <input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email:" className='w-[60%] border-2 duration-300 rounded-md border-slate-200 p-5 hover:border-slate-400 hover:bg-slate-100 focus:bg-slate-100'/>
                 <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password:" className='w-[60%] border-2 rounded-md duration-300 border-slate-200 p-5 hover:border-slate-400  hover:bg-slate-100 focus:bg-slate-100'/>
                 <button onClick={loginHandler} className='w-[60%] h-20 text-violet-500 border-violet-500 border-2 hover:border-violet-600 text-2xl font-normal disabled:cursor-not-allowed cursor-pointer disabled:hover:bg-violet-50 rounded-md disabled:bg-violet-100 enabled:hover:bg-violet-600 enabled:hover:text-gray-200 enabled:hover:-translate-y-1 enabled:hover:shadow-md duration-300'>Sign Up</button>
