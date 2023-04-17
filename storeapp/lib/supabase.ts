94848bef73f0beeb23f0183d0807aaaf4c9295b2
VisH317 <vt201916384@gmail.com>
2023-04-17 08:48:22 -0400

    supabase and react query setup

---
+++ storeapp/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+import { REACT_NATIVE_SUPABASE_ANON_KEY, REACT_NATIVE_SUPABASE_URL } from "../keys/keys";
+
+const supabase = createClient(REACT_NATIVE_SUPABASE_URL, REACT_NATIVE_SUPABASE_ANON_KEY)
+
+export default supabase
