b73da93d92075ec2854200a3b5d82fd4978a3952
VisH317 <vt201916384@gmail.com>
2023-04-18 10:56:04 -0400

    setup search results sorter

--- storeapp/src/lib/search.ts
+++ storeapp/src/lib/search.ts
@@ -1,4 +1,35 @@
+import { PostgrestSingleResponse } from "@supabase/supabase-js"
 import supabase from "./supabase"
+
+type StoreOutput = PostgrestSingleResponse<{
+    createdAt: string;
+    description: string | null;
+    id: string;
+    location: string;
+    name: string;
+    prompt: string;
+    userid: string;
+}>
+
+type StoreData = {
+    createdAt: string;
+    description: string | null;
+    id: string;
+    location: string;
+    name: string;
+    prompt: string;
+    userid: string;
+}
+
+const sorter = (row: StoreData , splitQuery: string[]) => {
+    const total: string = row.name + " " + row.location + " " + row.description
+    let ret: number = 0
+    splitQuery.map(q => {
+        if(total.includes(q)) ret++
+    })
+    return ret
+}
+
 
 // change the schema to provide the country, state, city, and address to search based on instead
 const search = async (query: string, country: string, state: string, city: string) => {
@@ -9,7 +40,14 @@ const search = async (query: string, country: string, state: string, city: strin
     const queryString = splitQuery.join(" | ")
 
     const rows = await supabase.from("Store").select().textSearch('title_description', queryString)
-    return rows
+    const data = rows.data
+    data?.sort((a: StoreData, b: StoreData): number => {
+        const aIdx: number = sorter(a, splitQuery)
+        const bIdx: number = sorter(b, splitQuery)
+        return aIdx > bIdx ? 1 : aIdx===bIdx ? 0 : -1
+    })
+
+    return data
 }
 
 export default search
