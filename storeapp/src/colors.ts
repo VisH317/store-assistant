4c86489852ac23aca3f6eea6a2921aa63af59a1a
VisH317 <54584201+VisH317@users.noreply.github.com>
2023-04-18 00:17:20 -0400

    home page setup

--- storeapp/src/colors.ts
+++ storeapp/src/colors.ts
@@ -2,7 +2,8 @@ const colors = {
     dark: "#0f172a",
     main: "#8b5cf6",
     light: "#cbd5e1",
-    medium: "#64748b"
+    medium: "#64748b",
+    verylight: "#e2e8f0"
 }
 
 export default colors
--- storeapp/src/pages/Home.tsx
+++ storeapp/src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image, Pressable, TouchableWithoutFeedback } from 'react-native'
 import colors from '../colors'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Home({ navigation }: any): JSX.Element {
     return (
         <View style={styles.container}>
+            <View style={{height: 64}}/>
             <View style={styles.imageContainer}>
-                <Image source={require("../../assets/undraw_shopping_app_flsj 1.png")}/>
+                <Image source={require("../../assets/shopping.png")} style={{ width: 200, height: 200 }}/>
             </View>
+            <View style={{height: 64}}/>
             <View style={styles.textContainer}>
                 <Text style={styles.titleText}>Store<Text style={styles.highlight}>GPT</Text></Text>
                 <Text style={styles.desc}>Your personal shopping assistant powered by AI tools</Text>
@@ -18,9 +20,13 @@ export default function Home({ navigation }: any): JSX.Element {
             <View style={styles.btnContainer}>
                 <Pressable style={styles.btn}>
                     <Text style={styles.btnText}>Find a Store</Text>
-                    {/* <FontAwesomeIcon icon={faArrowRight} size="lg" color="white"/> */}
+                    <View style={{flex: 1, justifyContent: "center", alignItems: "center", marginLeft: 7}}>
+                        <FontAwesomeIcon icon={faArrowRight} size={30} color="white"/>
+                    </View>
                 </Pressable>
             </View>
+            <View style={{height: 32}}/>
+            <View style={styles.tiltedRect}/>
         </View>
     )
 }
@@ -32,12 +38,26 @@ const styles = StyleSheet.create({
         padding: 10,
         fontFamily: "'Raleway', sans-serif",
         justifyContent: "center",
-        height: "100%"
+        height: "100%",
+        backgroundColor: "white"
+    },
+    tiltedRect: {
+        position: "absolute",
+        width: 700, 
+        height: 1000,
+        backgroundColor: colors.verylight,
+        transform: [{ rotate: '-10deg' }],
+        zIndex: -2,
+        left: -100,
+        top: 310
     },
     imageContainer: {
         paddingVertical: 10,
         paddingHorizontal: 3,
-        flexGrow: 0
+        flexGrow: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
     },
     textContainer: {
         flexGrow: 0,
@@ -88,5 +108,14 @@ const styles = StyleSheet.create({
         borderRightColor: colors.medium,
         paddingRight: 10,
         fontSize: 20
+    },
+    btnHover: {
+        color: "white",
+        backgroundColor: colors.dark,
+        display: "flex",
+        flexDirection: "row",
+        width: 170,
+        height: 50,
+        padding: 10
     }
 })
