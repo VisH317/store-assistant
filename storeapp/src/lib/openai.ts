a6f0d664a21179320f036df3664877fff435b0fd
VisH317 <vt201916384@gmail.com>
2023-04-19 10:03:41 -0400

    setup edge function interface

---
+++ storeapp/src/lib/openai.ts
@@ -0,0 +1,29 @@
+/**
+ * Supabase edge function to call the openai chatcompletion request
+ */
+
+type StoreCompletion = {
+    storePrompt: string,
+    question: string
+}
+
+type StoreRes = {
+    result: string
+}
+
+export default async function(storePrompt: string, question: string) {
+
+    const body: StoreCompletion = { storePrompt, question }
+
+    const res = await fetch("https://xtkqzukucrjotxyewlkk.functions.supabase.co/storeChat", {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(body)
+    })
+
+    const data: StoreRes = await res.json()
+    return data.result
+}
