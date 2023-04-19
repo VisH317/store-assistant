84c11f18b64f4dcd4a560eda2b7d1967539d1e3c
VisH317 <vt201916384@gmail.com>
2023-04-19 09:51:30 -0400

    setup function properly

--- supabase/functions/storeChat/index.ts
+++ supabase/functions/storeChat/index.ts
@@ -1,16 +1,20 @@
-import 'xhr_polyfill'
-import { serve } from 'https://deno.land/std@0.180.0/http/server.ts'
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { Configuration, OpenAIApi } from 'openai'
+
+console.log("Hello from Functions!")
+
+const config = new Configuration({
+    apiKey: Deno.env.get("OPENAI_API_KEY")
+})
+
+const openai = new OpenAIApi(config)
 
 serve(async (req) => {
     const { setupPrompt, question } = req.json()
-
-    const config = new Configuration({
-        apiKey: Deno.env.get("OPENAI_API_KEY")
-    })
-
-    const openai = new OpenAIApi(config)
-
     const messages: Message[] = [
         {
             role: "system",
@@ -45,3 +49,9 @@ type Message = {
     role: "user" | "assistant" | "system",
     content: string
 }
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
